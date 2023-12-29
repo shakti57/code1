02703570f072f54f819efd4a082d0360f9c29c97
Shakti Ranjan Mohanta <153294330+shakti57@users.noreply.github.com>
2023-12-29 08:51:10 +0530

    day 4 upload

---
+++ Day 4.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num=1
+print("numbers  squares")
+while(num<=10):
+    print(num,num**2)
+    
+    num=num+1
+
+
+# In[2]:
+
+
+#print following series 10,20,30,......300
+num=10
+while(num<=300):
+    print(num)
+    num=num+10
+    
+
+
+# In[3]:
+
+
+#print first 10 natural number in reverse order
+num=10
+while(num>=1):
+    print(num)
+    num=num-1
+
+
+# In[4]:
+
+
+# print sum of first 10 natural numbers
+num=10
+sum=0
+while(num>=1):
+    sum=sum+num
+    num=num-1
+print(sum)
+
+
+# In[5]:
+
+
+#print sum of first 10 even number
+num=2
+sum=0
+while(num<=20):
+    sum=sum+num
+    num=num+2
+print(sum)
+
+
+# In[ ]:
+
+
+
+
